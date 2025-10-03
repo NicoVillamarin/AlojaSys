141e03a46ebbb9d73ed374cd063508ff84bb96a1
Nicolas Villamarin <villamarin.nico@gmail>
2025-10-02 23:12:24 -0300

    last fixs

--- backend/apps/enterprises/views.py
+++ backend/apps/enterprises/views.py
@@ -9,7 +9,7 @@ class EnterpriseViewSet(viewsets.ModelViewSet):
     serializer_class = EnterpriseSerializer
 
     def get_queryset(self):
-        qs = Enterprise.objects.select_related("city", "city__state", "city__state__country").order_by("name")
+        qs = Enterprise.objects.select_related("city", "state", "country", "city__state", "city__state__country").order_by("name")
         country = self.request.query_params.get("country")
         if country:
             qs = qs.filter(city__state__country__code2=country.upper())
